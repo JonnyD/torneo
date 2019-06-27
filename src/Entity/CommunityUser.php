9d6c3fb8fee175331b945fef5145750f8831f3d4
Jonathan Devine <dev@jonnydevine.com>
2019-06-27 12:22:05 +0100

    Fixed type

--- src/Entity/CommunityUser.php
+++ src/Entity/CommunityUser.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\CommuniyUserRepository")
+ * @ORM\Entity(repositoryClass="App\Repository\CommunityUserRepository")
  */
 class CommunityUser
 {
